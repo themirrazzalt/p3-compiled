7796ff949f3a9e78469e9f46f3fee96d262e8060
themirrazzalt <123411289+themirrazzalt@users.noreply.github.com>
2023-01-26 23:19:11 +0000

    Add files via upload

--- node_api.js
+++ node_api.js
@@ -29,7 +29,7 @@ var global_api = {
                     client.onKilled();
                 }
             } catch {
-                returnl
+                return;
             }
         });
         ipc.of.mikep3.emit(`NewClient`, {
@@ -38,6 +38,56 @@ var global_api = {
                 port: port
             },
             reqid: Id
+        });
+    },
+    listenOnServer: function (port, cb) {
+        var Id = generate_req_id();
+        if(isNaN(port)) {
+            throw new Error('port needs to be a number')
+        }
+        ipc.of.mikep3.emit(`StartHosting`, {
+           reqid: Id,
+           port: port 
+        });
+        ipc.of.mikep3.on(`Server.ClientConnected_${Id}`, function (client) {
+            var pid = client.peerId;
+            var _c = {
+                emit: function (data) {
+                    ipc.of.mikep3.emit(
+                        `Server.EmitToClient:${pid}`,
+                        data
+                    );
+                },
+                onmessage: null,
+                ondisconnect: null,
+                address: client.address,
+                id: pid,
+                response_port: client.rpt,
+                rpt: client.rpt,
+                kill: function () {
+                    throw new Error('to-do');
+                }
+            };
+            var _gm = function (data) {
+                if(data.peerId !== pid) { return; }
+                try {
+                    _c.onmessage(data.message)
+                } catch (error) {
+                    null;
+                }
+            };
+            var _dk = function (data) {
+                if(data.peerId !== pid) { return; }
+                try {
+                    _c.ondisconnect()
+                } catch (error) {
+                    null;
+                }
+                ipc.of.mikep3.off(`Server.ClientMessage_${Id}`,_dk);
+                ipc.of.mikep3.off(`Server.ClientDisconnect_${Id}`,_gm);
+            };
+            ipc.of.mikep3.on(`Server.ClientMessage_${Id}`,_dk);
+            ipc.of.mikep3.on(`Server.ClientDisconnect_${Id}`,_gm);
         });
     }
 }
