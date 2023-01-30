686bbe92231d04877f3b6f8730908654c2a9e527
themirrazzalt <123411289+themirrazzalt@users.noreply.github.com>
2023-01-30 18:52:20 +0000

    Add files via upload

---
+++ node-api/node-ipc/node-ipc.js
@@ -0,0 +1,16 @@
+import IPC from './services/IPC.js';
+
+class IPCModule extends IPC{
+    constructor(){
+        super();
+    }
+
+    IPC=IPC;
+}
+
+const singleton=new IPCModule;
+
+export {
+    singleton as default,
+    IPCModule
+}
